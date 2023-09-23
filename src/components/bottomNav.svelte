c55c178b90f6c7e1004fccb16f483539a71913ed
Daniel Oglesby <dgo4man1@gmail.com>
2023-09-23 14:54:27 -0500

    feat: add github logo to navbar

---
+++ src/components/bottomNav.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import githubLogo from "../lib/assets/github-mark-white.svg";
+</script>
+
+<div class="btm-nav">
+  <a href="https://github.com/DanielOglesby">
+    <button>
+        <img src={githubLogo} alt="GitHub" class="h-5 w-5">
+    </button>
+  </a>
+  <button class="active">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-5 w-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      ><path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      /></svg
+    >
+  </button>
+  <button>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-5 w-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      ><path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+      /></svg
+    >
+  </button>
+</div>
