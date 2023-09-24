55bfe9049a2e104cdac3dded9ebe187461f73197
Daniel Oglesby <dgo4man1@gmail.com>
2023-09-23 20:57:43 -0500

    feat: navbar refactors

---
+++ src/components/TopNavbar.svelte
@@ -0,0 +1,19 @@
+<div class="flex items-center gap-4">
+  <div>
+    <button class="hover:text-sky-400 ease-in-out duration-200">
+      <a href="https://github.com/DanielOglesby"> About Me </a>
+    </button>
+  </div>
+  <div>
+    <button class="hover:text-sky-400 ease-in-out duration-200">
+      <a href="https://www.linkedin.com/in/daniel-oglesby-b68086b3/">
+        Skills
+      </a>
+    </button>
+  </div>
+  <div>
+    <button class="hover:text-sky-400 ease-in-out duration-200">
+      <a href="https://www.instagram.com/beltsquatguy/"> Experience </a>
+    </button>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,22 +5,33 @@
   import GithubButton from "../components/GithubButton.svelte";
   import LinkedInButton from "../components/LinkedInButton.svelte";
   import InstagramButton from "../components/InstagramButton.svelte";
+  import TopNavbar from "../components/TopNavbar.svelte";
+  import ButtonGroup from "../components/ButtonGroup.svelte";
 </script>
 
-<main class="p-10">
-  <div class="h-24 flex">
-    <div class="flex ml-4">
-      <Headshot />
+<main>
+  <section class="px-[15vw] py-10 flex">
+    <div class="h-24 flex w-full">
+      <div class="flex ml-4">
+        <Headshot />
+      </div>
+      <div class="flex ml-8 order-1">
+        <TopNavbar />
+      </div>
+      <div class="flex order-2 ml-auto">
+        <ButtonGroup />
+      </div>
     </div>
-    <div class="flex ml-4">
-      <GithubButton />
-    </div>
-    <div class="flex ml-4">
-      <LinkedInButton />
-    </div>
-    <div class="flex ml-4">
-      <InstagramButton />
-    </div>
+  </section>
+  <div class="flex p-10 justify-center">
+    <p class="text-center">
+      Hello! My name is <a
+        class="text-sky-400"
+        href="https://www.linkedin.com/in/daniel-oglesby-b68086b3/"
+        target="_blank">Daniel Oglesby</a
+      >. A software engineer from Dallas, TX. I'm passionate about building
+      beautiful, scalable applications.
+    </p>
   </div>
 </main>
 
