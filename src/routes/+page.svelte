588418939e9904466e555ad9d22adf185a9f80e3
Daniel Oglesby <dgo4man1@gmail.com>
2023-09-24 12:32:10 -0500

    feat: landing page typewriter text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
   import AnimatedText from "../components/LandingPage/AnimatedText.svelte";
 </script>
 
-<main class="px-[15vw] py-10">
+<main class="px-[15vw] py-10 flex flex-col">
   <Header></Header>
-  <div class="flex justify-center">
+  <div class="fixed inset-0 flex items-center justify-center">
     <AnimatedText />
   </div>
 </main>
