3e05367e4b5ca43d1cfe63c245f96d3bb7cdda43
Daniel Oglesby <dgo4man1@gmail.com>
2023-09-24 12:35:57 -0500

    feat: fix z index on typewriter text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
   import AnimatedText from "../components/LandingPage/AnimatedText.svelte";
 </script>
 
-<main class="px-[15vw] py-10 flex flex-col">
-  <Header></Header>
-  <div class="fixed inset-0 flex items-center justify-center">
+<main class="px-[15vw] py-10">
+  <div>
+    <Header></Header>
+  </div>
+  <div class="fixed inset-0 flex items-center justify-center -z-10">
     <AnimatedText />
   </div>
 </main>
